2b1a67b95e9550e35e56fe54e763ed88c074c46b
unknown <ameliaputriagustin20@gmail.com>
2022-03-03 12:00:17 +0700

    membuat headerr

--- src/App.js
+++ src/App.js
@@ -10,32 +10,31 @@ import icon2 from './image/iconn2.png';
 const App = () => {
   return (
     <div className="bgcolor">
-    <Container maxWidth="xs" style={{maxWidth: 444, backgroundColor: "white", paddingBottom : "80px"}}className="Container">
-      <div className="App">
-      <header > 
-        <Typography style={{fontSize: 10, fontWeight: 'bold', color: "#808080", fontFamily: "Roboto"}}>
-         Kamu berbelanja di
-        </Typography>
-        <div style={{display:'flex', alignItems: "center", marginTop: 5}}>
-          <img src={icon} alt="icon" style={{widht: 15, height: 15}}></img>
-          <Typography style={{ fontSize: 11, color: "black", fontWeight: 'bold', marginLeft : 5, fontFamily: "Roboto"}}>
-            Freshvo Cilandak (Jabodetabek)
-          </Typography>
-            <div style={{marginLeft: 155}}>
-            <Typography style={{ fontSize: 10, color: "#6495ED"}}>
-              Ubah
+      <Container maxWidth="xs" style={{maxWidth: 444, backgroundColor: "white", paddingBottom : "80px"}}className="Container">
+        <div className="App">
+          <header > 
+            <Typography style={{fontSize: 10, fontWeight: 'bold', color: "#808080", fontFamily: "Roboto"}}>
+              Kamu berbelanja di
             </Typography>
-            </div>
-            <div style={{marginLeft : 5}}>
-              <img src={icon1} alt="icon2" style={{widht: 20, height: 20}}></img>
-            </div>
+            <div style={{display:'flex', alignItems: "center", marginTop: 5}}>
+              <img src={icon} alt="icon" style={{widht: 15, height: 15}}></img>
+              <Typography style={{ fontSize: 11, color: "black", fontWeight: 'bold', marginLeft : 5, fontFamily: "Roboto"}}>
+                Freshvo Cilandak (Jabodetabek)
+              </Typography>
+              <div style={{marginLeft: 155}}>
+                <Typography style={{ fontSize: 10, color: "#6495ED"}}>
+                  Ubah
+                </Typography>
+              </div>
+              <div style={{marginLeft : 5}}>
+                <img src={icon1} alt="icon2" style={{widht: 20, height: 20}}></img>
+              </div>
               <div style={{paddingRight: 210, paddingLeft: 10 ,marginTop: 100, backgroundColor: '#f0f0f0', borderRadius: 20, position: "absolute", display: "flex", alignItems: "center", justifyContent: "center"}}>
                 <img src={icon2} alt="icon2" style={{widht: 20, height: 20, marginLeft: 5}}></img>
-            <InputBase style={{paddingBottom : 10, paddingTop: 10, fontSize: 12, fontFamily: "Roboto", marginLeft: 10}}placeholder="Cari produk.."/>
+                <InputBase style={{paddingBottom : 10, paddingTop: 10, fontSize: 12, fontFamily: "Roboto", marginLeft: 10}}placeholder="Cari produk.."/>
+              </div>
             </div>
-            </div>
-        
-      </header>
+        </header>
       </div>
     </Container>
     </div>
